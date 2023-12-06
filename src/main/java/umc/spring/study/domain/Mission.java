fc91a0cd8611e0fe56f9ff6af4ac2da2d678a450
yxhwxn <officeyh9819@gmail.com>
2023-12-06 16:19:34 +0900

    :sparkles: JPA 엔티티 생성

---
+++ src/main/java/umc/spring/study/domain/Mission.java
@@ -0,0 +1,35 @@
+package umc.spring.study.domain;
+
+import lombok.*;
+import umc.spring.study.domain.common.BaseEntity;
+import umc.spring.study.domain.mapping.MemberMission;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Mission extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer reward;
+
+    private LocalDate deadline;
+
+    private String missionSpec;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    @OneToMany(mappedBy = "mission", cascade = CascadeType.ALL)
+    private List<MemberMission> memberMissionList = new ArrayList<>();
+}
---
+++ src/main/java/umc/spring/study/domain/enums/MemberStatus.java
@@ -0,0 +1,5 @@
+package umc.spring.study.domain.enums;
+
+public enum MemberStatus {
+    ACTIVATE, INACTIVATE
+}
